585e087c40bfe67ac09fee161fd3cddd982b15be
David Padilla <david@crowdint.com>
2011-03-16 15:42:12 -0700

    Demotivational

--- sites.js
+++ sites.js
@@ -1,5 +1,6 @@
 var sites = [
   'http://www.crowdint.com',
   'http://www.time.is',
-  'http://twitter.com/#!/rails'
+  'http://twitter.com/#!/rails',
+  'http://www.flickr.com/search/show/?q=demotivational'
 ];
