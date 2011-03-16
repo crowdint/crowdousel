7b8b8403c82be17c32fde02424feb461d3d9797b
Chalo Fernandez <gonzalo.fernandez@crowdint.com>
2011-03-16 16:36:35 -0700

    Images by recent order

--- sites.js
+++ sites.js
@@ -2,7 +2,7 @@ var sites = [
   'http://www.crowdint.com',
   'http://search.twitter.com/search?q=ruby+rails',
   'http://www.time.is',
-  'http://www.flickr.com/search/show/?q=demotivational',
+  'http://www.flickr.com/search/show/?q=demotivational&s=rec',
   'http://search.twitter.com/search?q=crowdint',
   'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
   'http://192.168.1.120:3000',
