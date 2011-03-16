4bc3815604629938896b25d1a6a78dfe94d7f615
David Padilla <david@crowdint.com>
2011-03-16 15:49:57 -0700

    Crowdint Twitter search

--- sites.js
+++ sites.js
@@ -1,6 +1,8 @@
 var sites = [
   'http://www.crowdint.com',
+  'http://www.youtube.com/v/FMyGTqoaRcg&feature=fvst&autoplay=1',
   'http://www.time.is',
   'http://twitter.com/#!/rails',
-  'http://www.flickr.com/search/show/?q=demotivational'
+  'http://www.flickr.com/search/show/?q=demotivational',
+  'http://twitter.com/#search?q=crowdint'
 ];
