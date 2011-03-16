7b48e4559df6c63b28e0d8686ca04ec9c14c23ab
David Padilla <david@crowdint.com>
2011-03-16 17:23:38 -0600

    Fixed the twitter searches

--- sites.js
+++ sites.js
@@ -1,9 +1,12 @@
 var sites = [
   'http://www.crowdint.com',
+  'http://search.twitter.com/search?q=ruby+rails',
   'http://www.youtube.com/v/FMyGTqoaRcg&feature=fvst&autoplay=1',
   'http://www.time.is',
-  'http://twitter.com/#!/rails',
   'http://www.flickr.com/search/show/?q=demotivational',
-  'http://twitter.com/#search?q=crowdint',
-  'https://rubygems.org/gems/rails3-jquery-autocomplete/stats'
+  'http://search.twitter.com/search?q=crowdint',
+  'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
+  'http://192.168.1.120:3000',
+  'http://www.angelguardian.com.mx',
+  'http://www.macrumors.com'
 ];
