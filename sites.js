220fe5593b394ead1fec99d443ad894e21551074
Omar <ovargas27@gmail.com>
2011-04-01 13:38:57 -0600

    ad pages requested by Ana

--- sites.js
+++ sites.js
@@ -1,14 +1,16 @@
 var sites = [
   'http://www.crowdint.com',
+  'http://www.flickr.com/search/show/?q=granada&mt=photos&adv=1',
   'http://search.twitter.com/search?q=ruby+rails',
   'http://www.time.is',
   'http://search.twitter.com/search?q=crowdint',
   'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
   'http://192.168.1.120:3000',
   'http://www.angelguardian.com.mx',
+  'http://www.flickr.com/search/show/?q=modern+architecture&mt=photos&adv=1'
   'http://mediotiempo.com',
   'http://www.macrumors.com',
   'http://www.flickr.com/search/show/?q=motivational&s=rec',
-  'http://29.media.tumblr.com/tumblr_lhjnf4angR1qe2axoo1_500.gif',
-  'http://news.ycombinator.com/'
+  'http://news.ycombinator.com/',
+  'http://www.flickr.com/search/show/?q=white+sands&ct=0&mt=photos&adv=1'
 ];
