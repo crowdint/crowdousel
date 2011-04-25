5fc18ce61d5cc6510dcf2f805717debad1270507
Mario Zaizar <mariozaizar@gmail.com>
2011-04-25 14:30:46 -0700

    Seems like some flickr urls are wrong..

--- sites.js
+++ sites.js
@@ -2,19 +2,18 @@ var sites = [
   'http://www.crowdint.com',
   'http://ci.crowdint.com',
   'http://www.magmarails.com',
-  'http://www.flickr.com/search/show/?q=granada&mt=photos&adv=1',
+  'http://www.flickr.com/search/show/?q=motivational+poster',
   'http://search.twitter.com/search?q=ruby+rails',
   'http://www.time.is',
   'http://search.twitter.com/search?q=crowdint',
   'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
   'http://192.168.1.120:3000',
   'http://www.angelguardian.com.mx',
-  'http://www.flickr.com/search/show/?q=modern+architecture&mt=photos&adv=1',
+  'http://www.flickr.com/search/show/?q=modern+architecture',
   'http://mediotiempo.com',
   'http://www.macrumors.com',
-  'http://www.flickr.com/search/show/?q=motivational&s=rec',
+  'http://www.flickr.com/search/show/?q=demotivational+poster',
   'http://news.ycombinator.com/',
   'http://search.twitter.com/search?q=magmarails',
-  'http://www.flickr.com/search/show/?q=white+sands&ct=0&mt=photos&adv=1',
-  'http://www.rallyonrails.com'
+  'http://www.flickr.com/search/show/?q=white+sands'
 ];
