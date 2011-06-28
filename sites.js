c3e89491f81ad74165e8cd5cfda0862cf16c1807
Edwin Cruz <softr8@gmail.com>
2011-06-28 10:05:30 -0700

    Adding CI server

--- sites.js
+++ sites.js
@@ -1,15 +1,21 @@
 var sites = [
   'http://www.crowdint.com',
   'http://www.magmarails.com',
+  'http://ci.crowdint.com',
   'http://www.flickr.com/search/show/?q=motivational+poster',
   'http://search.twitter.com/search?q=ruby+rails',
+  'http://ci.crowdint.com',  
   'http://www.time.is',
   'http://search.twitter.com/search?q=crowdint',
+  'http://ci.crowdint.com',
   'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
   'http://www.angelguardian.com.mx',
+  'http://ci.crowdint.com',
   'http://mediotiempo.com',
   'http://www.macrumors.com',
+  'http://ci.crowdint.com',
   'http://www.flickr.com/search/show/?q=demotivational+poster',
   'http://search.twitter.com/search?q=magmarails',
-  'http://www.flickr.com/search/show/?q=white+sands'
+  'http://www.flickr.com/search/show/?q=white+sands',
+  'http://ci.crowdint.com'
 ];
