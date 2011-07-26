c1c8494ead795e4ad4ee373b4a876469f2d6edee
Mario Zaizar <mariozaizar@gmail.com>
2011-07-25 17:56:32 -0700

    Adding some specific folders to the Crowd's flickr slideout pages.

--- sites.js
+++ sites.js
@@ -2,14 +2,16 @@ var sites = [
   'http://www.crowdint.com',
   'http://www.magmarails.com',
   'http://ci.crowdint.com',
+  'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
   'http://time.is',
 //  'http://remember-me.heroku.com',
-  'http://www.flickr.com/photos/63319310@N04/show/',
+  'http://www.flickr.com/photos/63319310@N04/show', 
   'http://www.flickr.com/search/show/?q=motivational+poster',
-  'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
+  'http://www.flickr.com/photos/63319310@N04/sets/72157627157024979/show', // Photoshopista
+  'http://www.flickr.com/search/show/?q=demotivational+poster',
+  'http://www.flickr.com/photos/63319310@N04/sets/72157627281344070/show', // We're crowd
 //  'http://www.angelguardian.com.mx',
 //  'http://mediotiempo.com',
-  'http://www.macrumors.com',
-  'http://www.flickr.com/search/show/?q=demotivational+poster',
+//  'http://www.macrumors.com'
 ];
 
