fc83eccf917c1928d3b0eb091fa94ba08f25b96c
Mario Zaizar <mariozaizar@gmail.com>
2011-07-25 17:57:06 -0700

    Fixing Typo

--- sites.js
+++ sites.js
@@ -5,11 +5,11 @@ var sites = [
   'https://rubygems.org/gems/rails3-jquery-autocomplete/stats',
   'http://time.is',
 //  'http://remember-me.heroku.com',
-  'http://www.flickr.com/photos/63319310@N04/show', 
+  'http://www.flickr.com/photos/63319310@N04/show', // Most recent photos
   'http://www.flickr.com/search/show/?q=motivational+poster',
   'http://www.flickr.com/photos/63319310@N04/sets/72157627157024979/show', // Photoshopista
   'http://www.flickr.com/search/show/?q=demotivational+poster',
-  'http://www.flickr.com/photos/63319310@N04/sets/72157627281344070/show', // We're crowd
+  'http://www.flickr.com/photos/63319310@N04/sets/72157627281344070/show' // We're crowd
 //  'http://www.angelguardian.com.mx',
 //  'http://mediotiempo.com',
 //  'http://www.macrumors.com'
