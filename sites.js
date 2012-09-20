c98eec905fac018600c9169c824daa29fb8f99a4
David Padilla <david@crowdint.com>
2012-09-20 15:19:01 -0500

    Remove the analytics links, increase time

--- sites.js
+++ sites.js
@@ -7,11 +7,9 @@ var sites = [
   'https://www.google.com/analytics/web/?hl=en&pli=1#report/visitors-overview/a31503719w58295858p59503997/',
   'http://www.flickr.com/search/show/?q=motivational+poster',
   'http://www.flickr.com/photos/63319310@N04/sets/72157627157024979/show', // Photoshopista
-  'https://www.google.com/analytics/web/?hl=en&pli=1#realtime/rt-overview/a17527068w35471312p34831856/',
   'http://www.flickr.com/search/show/?q=demotivational+poster',
   'http://www.flickr.com/photos/63319310@N04/sets/72157627281344070/show', // We're crowd
   'http://www.angelguardian.com.mx',
   'http://www.macrumors.com',
-  'https://www.google.com/analytics/web/?hl=en#dashboard/default/a17527068w35471312p34831856/',
-  'https://www.google.com/analytics/web/?hl=en&pli=1#realtime/rt-overview/a17527068w35471312p34831856/'
+  'http://visibletweets.com/#query=Crowdint&animation=2',
 ];
