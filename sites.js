e2d23250428cf918038b22c43c572dd73c544e1f
David Padilla <david@crowdint.com>
2012-09-20 15:02:02 -0500

    Updated website list

--- sites.js
+++ sites.js
@@ -3,12 +3,15 @@ var sites = [
   'http://www.magmarails.com',
   'http://time.is',
   'http://remember-me.heroku.com',
-  'https://picasaweb.google.com/108372401803573113438/Wallpapers#slideshow/5645600912256954290',
   'http://www.flickr.com/photos/63319310@N04/show', // Most recent photos
+  'https://www.google.com/analytics/web/?hl=en&pli=1#report/visitors-overview/a31503719w58295858p59503997/',
   'http://www.flickr.com/search/show/?q=motivational+poster',
   'http://www.flickr.com/photos/63319310@N04/sets/72157627157024979/show', // Photoshopista
+  'https://www.google.com/analytics/web/?hl=en&pli=1#realtime/rt-overview/a17527068w35471312p34831856/',
   'http://www.flickr.com/search/show/?q=demotivational+poster',
   'http://www.flickr.com/photos/63319310@N04/sets/72157627281344070/show', // We're crowd
   'http://www.angelguardian.com.mx',
-  'http://www.macrumors.com'
+  'http://www.macrumors.com',
+  'https://www.google.com/analytics/web/?hl=en#dashboard/default/a17527068w35471312p34831856/',
+  'https://www.google.com/analytics/web/?hl=en&pli=1#realtime/rt-overview/a17527068w35471312p34831856/'
 ];
